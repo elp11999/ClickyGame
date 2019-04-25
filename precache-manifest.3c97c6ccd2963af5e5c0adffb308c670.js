ccf893bbafd7daf99fbd0fe289e619be7f072780
Michael Henderson <mhenderson557@yahoo.com>
2019-04-24 22:30:08 -0500

    Updates

---
+++ precache-manifest.3c97c6ccd2963af5e5c0adffb308c670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "dbfd5f383acf4c782a7e",
+    "url": "/clickygame/static/js/main.4e589764.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "dbfd5f383acf4c782a7e",
+    "url": "/clickygame/static/css/main.f3fe868d.chunk.css"
+  },
+  {
+    "revision": "a74976f7d0a5bcf07d6226725981ac57",
+    "url": "/clickygame/index.html"
+  }
+];
